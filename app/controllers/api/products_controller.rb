a1ebfda86641e30bb4c03e91750e6284957930fd
Jeneen <jeneen.lemar@gmail.com>
2020-05-30 20:28:37 -0400

    adds more routes and controller actions

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -6,4 +6,19 @@ def all_products_action
   
 end
 
+def one_product_action
+  @product = Product.first
+  render "one_product.json.jb"
+  
+end
+
+def any_product_action
+  @product = Product.find_by(id: params["id"])
+  #alternate method to find
+  # product_id = params[:id]
+  # @product = Product.find_by(id: product_id)
+  render "any_product.json.jb"
+  
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "all_products" => "products#all_products_action"
+    get "one_product" =>"products#one_product_action"
+    get "any_product" =>"products#any_product_action"
+    get "any_product/:id" =>"products#any_product_action"
   end
 end
